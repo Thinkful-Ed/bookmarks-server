c95320db978a73dd6e279aef3541a3e833d80a17
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-02-02 22:38:53 +0000

    first commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+
+const app = express()
+
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
+app.use(cors())
+app.use(helmet())
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
+
+module.exports = app
---
+++ test/setup.js
@@ -0,0 +1,7 @@
+process.env.NODE_ENV = 'test'
+
+const { expect } = require('chai')
+const supertest = require('supertest')
+
+global.expect = expect
+global.supertest = supertest
