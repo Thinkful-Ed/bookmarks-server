205605f371bf32af64f96854205fc2168441fb7a
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-02-04 13:11:28 +0000

    implements POST and DELETE using database with XSS sanitized responses

--- test/bookmarks-fixtures.js
+++ test/bookmarks-fixtures.js
@@ -24,6 +24,26 @@ function makeBookmarksArray() {
   ]
 }
 
+function makeMaliciousBookmark() {
+  const maliciousBookmark = {
+    id: 911,
+    title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    url: 'https://www.hackers.com',
+    description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+    rating: 1,
+  }
+  const expectedBookmark = {
+    ...maliciousBookmark,
+    title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+    description: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
+  }
+  return {
+    maliciousBookmark,
+    expectedBookmark,
+  }
+}
+
 module.exports = {
   makeBookmarksArray,
+  makeMaliciousBookmark,
 }
