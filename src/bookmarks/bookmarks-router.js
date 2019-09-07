5c3de264cd7e31b54e4c2a06da3ab01603724665
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-09-07 14:12:08 +0100

    allow string values of numbers between 0 and 5 for rating in POST /bookmark

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -36,7 +36,9 @@ bookmarksRouter
 
     const { title, url, description, rating } = req.body
 
-    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+    const ratingNum = Number(rating)
+
+    if (!Number.isInteger(ratingNum) || ratingNum < 0 || ratingNum > 5) {
       logger.error(`Invalid rating '${rating}' supplied`)
       return res.status(400).send({
         error: { message: `'rating' must be a number between 0 and 5` }
