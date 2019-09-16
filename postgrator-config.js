5edda5c60b5c575a9f685cee0b15de35ecb17370
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-09-16 15:20:25 +0100

    using dunder_mifflin and added a `npm run migrate:test` command

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DB_URL
+    : process.env.DB_URL,
 }
--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/bookmarks',
+  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
 }
